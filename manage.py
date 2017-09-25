50d02dd0f120c0e8f62e8d372ee5eb9a87d82486
Arnav Jain <archiejain1021@gmail.com>
2017-09-25 13:09:18 -0500

    Fix syntax error

--- manage.py
+++ manage.py
@@ -105,11 +105,11 @@ def add_dues():
 			dues = int(input("How much dues do you want to reset it to? "))
 			member.dues = dues
 			db.session.commit()
-			
+
 		answer = input("Do you want to continue (y/n)? ")
 		if answer == 'y':
 			addDues = True
-		else
+		else:
 			addDues = False
 
 @manager.command
